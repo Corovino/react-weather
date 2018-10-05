f002d7219ab1d9c30597427fd359b8c61537b4cb
Hebert Romero <hjr@iqthink.com>
2018-10-04 22:01:30 -0500

    ajustes generales funcionalidad  get data

--- src/Components/Wheather.js
+++ src/Components/Wheather.js
@@ -1,24 +1,32 @@
 import React, { Component } from 'react';
 
-class Weather extends Component {
-
-    render() {
-
-        return (
-            
-               <div>    
-                <p> Location : {this.props.country}, {this.props.city}</p>
-                <p>Temperature: {this.props.temperature} </p>
-                <p>Humidity : {this.props.humidity} </p> 
-                <p>Conditions: {this.props.description} </p>      
-               </div>
-            
-        );
-
-    }
-
-
-};
+const Weather = props => (
+    <div className="weather__info">
+        {
+            props.city && props.country && <p className="weather__key"> Location:
+	 		<span className="weather__value"> {props.city}, {props.country}</span>
+            </p>
+        }
+        {
+            props.temperature && <p className="weather__key"> Temperature:
+	 		<span className="weather__value"> {props.temperature}	</span>
+            </p>
+        }
+        {
+            props.humidity && <p className="weather__key"> Humidity:
+	 		<span className="weather__value"> {props.humidity} </span>
+            </p>
+        }
+        {
+            props.description && <p className="weather__key"> Conditions:
+	 		<span className="weather__value"> {props.description} </span>
+            </p>
+        }
+        {
+            props.error && <p className="weather__error">{props.error}</p>
+        }
+    </div>
+);
 
 export default Weather;
 
